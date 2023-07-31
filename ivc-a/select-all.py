4f1fa8918d3d2a96cda7d2ad11bc5fd32772fed8
vctrmn <vctrmn>
2023-07-31 12:33:43 +0000

    Change file to sql

--- ivc-a/select-all.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC ## IVC A - Demo of Databrick Repo
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC SELECT * FROM sncf_ter_sample_database.sample_csv_file
