f442d31fa7e5f35a124fd7687a78ebabba475bb5
vctrmn <vctrmn>
2023-07-31 12:31:25 +0000

    init select-all

---
+++ ivc-a/select-all.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## IVC A - Demo of Databrick Repo
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM sncf_ter_sample_database.sample_csv_file
