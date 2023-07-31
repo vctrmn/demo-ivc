c3c0f7df4d9e09ef6b8fe34fa83bd8c5f0025005
vctrmn <vctrmn>
2023-07-31 13:17:24 +0000

    Add IVC B

---
+++ ivc-b/select-origin-destination.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ## IVC B - Demo of Databrick Repo
+
+-- COMMAND ----------
+
+SELECT origin, destination FROM sncf_ter_sample_database.sample_csv_file
